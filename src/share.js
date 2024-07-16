5687b93c77f4b4f6519030c1b6e1b3e3ff191a64
MuhammetKoca2810 <muhammetkoca2810@gmail.com>
2024-07-16 09:26:46 -0500

    cors test

--- src/share.js
+++ src/share.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const route = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const cors = require("cors");
+
 app.use(cors());
+
+app.use(express.json());
+
+const route = express.Router();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 module.exports = {
   app,
